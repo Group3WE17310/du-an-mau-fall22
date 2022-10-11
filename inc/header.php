939923a53083abde23e20b63ac108138a8133390
VuHuuQuan1692003 <115264912+VuHuuQuan1692003@users.noreply.github.com>
2022-10-12 00:02:16 +0700

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/header.css">
+</head>
+<style>
+    header h1{
+    padding: 10px;
+    background-color: rgb(149, 218, 149);
+    color: darkgreen;
+    margin: 10px 0;
+    }
+    nav{
+    background-color: black;
+    padding: 10px;
+    margin-bottom: 10px;
+    }
+    
+    nav a {
+    color: gray;
+    margin: 5px;
+    }
+    nav a:hover{
+    font-size: 20px;
+    }
+</style>
+<body>
+<header>
+
+<h1>Siêu thị trực tuyến</h1>
+
+<nav>
+    <a href="home.php">Trang chủ</a>
+    <a href="">Giới thiệu</a>
+    <a href="">Liên hệ</a>
+    <a href="">Góp ý</a>
+    <a href="">Hỏi đáp</a>
+</nav>
+</header>
+</body>
+</html>
+
+    
+
+        
+
+
+
+        
