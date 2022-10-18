3008d048993206792f480fd5e452402bb6470ff9
datlqph24221 <115266622+datlqph24221@users.noreply.github.com>
2022-10-19 00:00:16 +0700

    Add files via upload

--- dao/binh_luan.php
+++ dao/binh_luan.php
@@ -31,5 +31,9 @@
         $sql = "SELECT count(*) FROM binh_luan WHERE ma_bl=?";
         return pdo_query_value($sql, $ma_bl) > 0;
        }
+       function binh_luan_select_by_hang_hoa($ma_hh){
+        $sql = "SELECT b.*, h.ten_hh FROM binh_luan b JOIN hang_hoa h ON h.ma_hh=b.ma_hh WHERE b.ma_hh=? ORDER BY ngay_bl DESC";
+        return pdo_query($sql, $ma_hh);
+       }
                             
 ?>
